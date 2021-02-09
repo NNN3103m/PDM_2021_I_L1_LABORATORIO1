ee2d4202e8e6e3848929e41bdf9b7a76acf6b2cf
NSTR3103 <nnn3103m@gmail.com>
2021-02-09 13:40:20 -0600

    Crear las activitys e interconectarlas

---
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_l1_laboratorio1/FinalizadoActivity.kt
@@ -0,0 +1,11 @@
+package hn.edu.ujcv.pdm_2021_i_l1_laboratorio1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FinalizadoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finalizado)
+    }
+}
