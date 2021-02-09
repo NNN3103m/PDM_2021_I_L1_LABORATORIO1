23da39d80036f3663494fe4cd8d8cf8454a70e97
NSTR3103 <nnn3103m@gmail.com>
2021-02-09 01:18:26 -0600

    Initial commit

---
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_l1_laboratorio1/CobrarSanaaActivity.kt
@@ -0,0 +1,11 @@
+package hn.edu.ujcv.pdm_2021_i_l1_laboratorio1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CobrarSanaaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cobrar_sanaa)
+    }
+}
---
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_l1_laboratorio1/RetirarDineroActivity.kt
@@ -0,0 +1,11 @@
+package hn.edu.ujcv.pdm_2021_i_l1_laboratorio1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RetirarDineroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_retirar_dinero)
+    }
+}
