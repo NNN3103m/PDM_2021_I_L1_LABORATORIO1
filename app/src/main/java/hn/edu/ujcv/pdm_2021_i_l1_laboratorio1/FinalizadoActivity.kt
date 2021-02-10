0fc5f9a1ba615ae45a237e73e767553ac3d843f8
NSTR3103 <nnn3103m@gmail.com>
2021-02-09 23:31:03 -0600

    RetirarDineroActivity ya esta solo hace falta la validacion del rango de numeros que necesita

--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_l1_laboratorio1/FinalizadoActivity.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_l1_laboratorio1/FinalizadoActivity.kt
@@ -2,10 +2,13 @@ package hn.edu.ujcv.pdm_2021_i_l1_laboratorio1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_finalizado.*
+import kotlinx.android.synthetic.main.activity_retirar_dinero.*
 
 class FinalizadoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finalizado)
+        btnFelicidadesRegresar.setOnClickListener{onBackPressed()}
     }
 }
--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_l1_laboratorio1/RetirarDineroActivity.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_l1_laboratorio1/RetirarDineroActivity.kt
@@ -4,11 +4,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_cobrar_enee.*
 import kotlinx.android.synthetic.main.activity_retirar_dinero.*
+import android.widget.Toast
+import android.content.Intent
 
 class RetirarDineroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_retirar_dinero)
         btnRegresarRetirarDinero.setOnClickListener{onBackPressed()}
+        btnRetirarDineroEnviar.setOnClickListener{enviar()}
+    }
+    fun enviar(){
+        if(txtMonto.text.toString().isEmpty() && txtCuenta.text.toString().isEmpty()){
+            Toast.makeText(this,"Debe Ingresar un monto y una cuenta.", Toast.LENGTH_SHORT).show()
+        }
+        else if (txtMonto.text.toString().isNotEmpty() && txtCuenta.text.toString().isEmpty()){
+            Toast.makeText(this,"Debe Ingresar una cuenta.", Toast.LENGTH_SHORT).show()
+        }
+        else if (txtMonto.text.toString().isEmpty() && txtCuenta.text.toString().isNotEmpty()){
+            Toast.makeText(this,"Debe Ingresar un monto.", Toast.LENGTH_SHORT).show()
+        }
+        else{
+            val intent = Intent(this, FinalizadoActivity::class.java)
+            //intent.putExtra("nombre",txtNombre.text.toString())
+            startActivity(intent)
+        }
     }
 }
